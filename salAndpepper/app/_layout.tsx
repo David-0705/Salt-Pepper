275eea425d36026ee156785ec3e2db96621fc0e9
Shaunm1724 <shaun.menezes1724@gmail.com>
2025-03-02 10:22:34 +0530

    Removed Header from pages

--- salAndpepper/app/_layout.tsx
+++ salAndpepper/app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-  <Stack.Screen name="community" />
-  <Stack.Screen name="area-ranking" />
-  <Stack.Screen name="login" />
-  <Stack.Screen name="register" />
-  <Stack.Screen name="CrimeReport"/>
-  <Stack.Screen name="CrimeInfo"/>
+  return <Stack
+  screenOptions={{
+    // Hide the header for all other routes.
+    headerShown: false,
+  }}
+  >
+    <Stack.Screen name="community" options={{ headerShown: false }} />
+    <Stack.Screen name="area-ranking" options={{ headerShown: false }} />
+    <Stack.Screen name="login" options={{ headerShown: false }} />
+    <Stack.Screen name="register" options={{ headerShown: false }} />
+    <Stack.Screen name="CrimeReport" options={{ headerShown: false }} />
+    <Stack.Screen name="CrimeInfo" options={{ headerShown: false }} />
   </Stack>;
 }
